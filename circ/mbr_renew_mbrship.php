32723177c23adeb818097a970f05465fb857a1bf
HanseGucker <hansegucker@gmail.com>
2017-11-24 20:33:16 +0100

    Add files from german project (v0.7.2.2)

---
+++ circ/mbr_renew_mbrship.php
@@ -0,0 +1,75 @@
+<?php
+/* This file is part of a copyrighted work; it is distributed with NO WARRANTY.
+ * See the file COPYRIGHT.html for more details.
+ */
+
+  session_cache_limiter(null);
+
+  $restrictToMbrAuth = TRUE;
+  require_once("../shared/common.php");
+  require_once("../functions/inputFuncs.php");
+  require_once("../functions/formatFuncs.php");
+  require_once("../shared/logincheck.php");
+
+  require_once("../classes/Member.php");
+  require_once("../classes/MemberQuery.php");
+  require_once("../classes/BiblioSearch.php");
+  require_once("../classes/BiblioSearchQuery.php");
+  require_once("../classes/BiblioHold.php");
+  require_once("../classes/BiblioHoldQuery.php");
+  require_once("../classes/MemberAccountQuery.php");
+  require_once("../classes/DmQuery.php");
+  require_once("../functions/errorFuncs.php");
+  require_once("../classes/Localize.php");
+  $loc = new Localize(OBIB_LOCALE,"circulation");
+
+  if (count($_GET) == 0) {
+    header("Location: ../circ/index.php");
+    exit();
+  }
+
+  if (isset($_GET["mbrid"])){
+    unset($_SESSION["postVars"]);
+    unset($_SESSION["pageErrors"]);
+    #****************************************************************************
+    #*  Retrieving get var
+    #****************************************************************************
+    $mbrid = $_GET["mbrid"];
+    $length = $_GET["length"];
+
+    #****************************************************************************
+    #*  Search database
+    #****************************************************************************
+    $mbrQ = new MemberQuery();
+    $mbrQ->connect();
+    $mbr = $mbrQ->get($mbrid);
+    $mbrQ->close();
+  
+  if (($mbr->getMembershipEnd()!="0000-00-00") and (strtotime($mbr->getMembershipEnd())>strtotime("now"))) {
+    $year = substr($mbr->getMembershipEnd(), 0, 4);
+    $month = (int)substr($mbr->getMembershipEnd(), 5, 2);
+    $day = (int)substr($mbr->getMembershipEnd(), 8, 2);
+    $month = $month + $length;
+    $newdate = date("Y-m-d",mktime(0,0,0,$month,$day,$year));
+    $mbr->setMembershipEnd($newdate);
+  } else {
+    $temp=$neu = strtotime("+$length month");
+    $newdate = date("Y-m-d",$temp);
+    $mbr->setMembershipEnd($newdate);
+  }
+
+  $mbrQ->update($mbr);  
+  $mbrQ->close();
+
+  #**************************************************************************
+  #*  Destroy form values and errors
+  #**************************************************************************
+  unset($_SESSION["postVars"]);
+  unset($_SESSION["pageErrors"]);
+
+  $msg = $loc->getText("mbrRenewSuccess",array("length"=>$length));
+  $msg = urlencode($msg);
+  header("Location: ../circ/mbr_view.php?mbrid=".$mbr->getMbrid()."&reset=Y&msg=".$msg);
+  exit();
+}
+?> 
---
+++ locale/de/help/contents.php
@@ -0,0 +1,26 @@
+<h1>Inhalte:</h1>
+<ol>
+  <li>OPAC und Suchergebnisse</li>
+    <ol type="a">
+      <li><a href="../shared/help.php?page=opac">Onlinekatalog (OPAC)</a></li>
+      <li><a href="../shared/help.php?page=biblioSearch">Suchergebnisse</a></li>
+    </ol>
+  <li>Generelle Konzepte der Bibliotheksverwaltung</li>
+    <ol type="a">
+      <li><a href="../shared/help.php?page=status">Status-Änderungen der Medien verstehen</a></li>
+      <li><a href="../shared/help.php?page=barcodes">Barcodes verstehen</a></li>
+    </ol>
+  <li>Hilfe zur Ausleihe</li>
+    <ol type="a">
+      <li><a href="../shared/help.php?page=circulation">Mitgliedersuche (Startseite der Ausleihe)</a></li>
+      <li><a href="../shared/help.php?page=memberView">Mitgliederinfo</a></li>
+      <li><a href="../shared/help.php?page=opacLookup">Barcodesuche</a></li>
+      <li><a href="../shared/help.php?page=checkin">Medienrückgabe</a></li>
+    </ol>
+  <li>Hilfe zur Katalogisierung</li>
+    <ol type="a">
+      <li><a href="../shared/help.php?page=biblioEdit">Neue Medien und Medien editieren</a></li>
+      <li><a href="../shared/help.php?page=biblioCopyEdit">Neues Exemplar und Exemplar editieren</a></li>
+    </ol>
+  
+</ol>
